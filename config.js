c483b81c82639f908cf8fd55e59480e182f208ea
Anna Sharuntsova <Anna.Sharuntsova@confirmit.com>
2021-06-05 22:34:11 +0300

    config.js init

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+    urls: {
+        currencyRates: 'https://iss.moex.com/iss/statistics/engines/futures/markets/indicativerates/securities.json'
+    },
+    files: {
+        currencyRates: './data-storage.json',
+        log: './data/log/changes-log-storage.json'
+    }
+}
