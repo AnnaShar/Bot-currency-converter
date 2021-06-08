bb47b0906ea1d0b0dab43ffe4da8feb0f82bca32
Anna Sharuntsova <Anna.Sharuntsova@confirmit.com>
2021-06-08 23:41:15 +0300

    Fill list of commands and their texts. Texts are need to be checked.

--- commands.js
+++ commands.js
@@ -1,30 +1,82 @@
+//TODO recheck all texts
+
 export default {
-    help: {
-        name: '/help'
+    customCommands: ['conversions', 'currencies', 'language'],
+
+    start: {
+        text: {
+            eng: 'I can convert currencies by MOEX rates.\n' +
+                'To see available conversions press /conversions.\n' +
+                'To see all available actions press /help.',
+            ru: 'Я конвертирую валюту по курсу закрытия прошедших торгов на Мосбирже.\n' +
+                'Список доступных конвертаций можно посмотреть, используя команду /conversions.\n' +
+                'Список доступных команд вы найдете в разделе /help.\n' +
+                'Буду рад помочь!'
+        },
+        hello: {
+            eng: 'Hello',
+            ru: 'Привет'
+        }
     },
-    conversions:{
+
+    help: {
+        name: '/help',
+        text: {
+            eng: 'Here is the list of actions you can do. \n',
+            ru: 'Вам доступны следующие действия. \n'
+        }
+    },
+    conversions: {
         name: '/conversions',
         description: {
             eng: 'show all available conversions',
             ru: 'показывает все возможные конвертации'
         },
-        text:{
+        text: {
             eng: 'Choose what conversion you want to make.\nIf you do not know currencies abbreviations, press /currencies.\n',
             ru: 'Выберите способ конвертации.\nЕсли вы не знаете сокращенные наименования валют, нажмите /currencies.\n'
         }
     },
-    language:{
+    language: {
         name: '/language',
         description: {
             eng: 'let choose different language',
             ru: 'дает выбрать другой язык интерфейса'
+        },
+        text: {
+            eng: 'Choose interface language from below.',
+            ru: 'Выберите язык интерфейса из представленных ниже.'
         }
     },
-    mode:{
+    currencies: {
         name: '/currencies',
         description: {
             eng: 'shows list of available currencies names',
             ru: 'показывает список доступных валют и их расшифровки'
+        }
+    },
+    changeLanguage: {
+        text: {
+            eng: 'You switched interface language to English.',
+            ru: 'Вы поменяли язык интерфейса на русский.'
+        }
+    },
+    saveConversion: {
+        text: {
+            eng: 'Enter a number to convert',
+            ru: 'Введите число для конвертации'
+        }
+    },
+    convert:{
+        failedConversion: {
+            eng:'Please, choose what do you need to convert.\n' +
+                'The list of available conversions is below.\n',
+            ru:'Пожалуйста, выберите, что вы хотите конвертировать. \n' +
+                'Список доступных конвертаций ниже. \n'
+        },
+        invalidNumber: {
+            eng:'Please, enter a valid number.',
+            ru:'Пожалуйста, введите валидное число.'
         }
     }
 }
