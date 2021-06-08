6d1024d7200baf282ec63e63f240614d9fa32ab0
Anna Sharuntsova <Anna.Sharuntsova@confirmit.com>
2021-06-08 23:42:32 +0300

    Extract functions making lists of constants.

---
+++ constants.js
@@ -0,0 +1,35 @@
+//TODO think of better file naming
+import {readFile} from "./utils/file-reader-writer.js";
+import config from "./config.js";
+
+let currencyRates = null;
+
+const getCurrencyRates = () => {
+    if (!currencyRates) {
+        currencyRates = readFile(config.files.dataStorage.currency);
+    }
+    return currencyRates;
+};
+
+const getConversionsList = () => {
+    const rates = getCurrencyRates();
+    const availableConversions = Object.values(rates);
+    let conversionsList = [];
+    availableConversions.forEach((conversion) => {
+        const from = conversion.from;
+        const to = conversion.to;
+        const conversionName = `${from}_to_${to}`;
+        conversionsList.push(conversionName);
+    });
+    return conversionsList;
+};
+
+const getLanguageList = () => {
+    return ['eng', 'ru'];
+};
+
+export default {
+    getLanguageList: getLanguageList,
+    getConversionsList:getConversionsList,
+    getCurrencyRates: getCurrencyRates
+}
