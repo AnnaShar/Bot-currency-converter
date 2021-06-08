bc10b378480a9a989fcb001df70e10403fe0a715
Anna Sharuntsova <Anna.Sharuntsova@confirmit.com>
2021-06-08 23:44:14 +0300

    Almost finished commands implementations. Converts doesn't work

--- index.js
+++ index.js
@@ -1,81 +1,49 @@
 import keys from './keys.js';
 import telegrafResponse from './telegraf-request-handler.js';
 import {Telegraf} from 'telegraf';
+import constants from "./constants.js";
 
 const bot = new Telegraf(keys.telegramAPIKey);
 
-
-let conversion;
-
-bot.start(ctx => {
-    ctx.reply(
-        telegrafResponse.start(ctx.update.message.from)
-    )
-});
-
-bot.help(ctx => ctx.reply(
-    '/conversions'
+bot.start(ctx => ctx.reply(
+    telegrafResponse.start(ctx.update.message.from)
 ));
 
+bot.help(ctx => ctx.reply(
+    telegrafResponse.help()
+));
 
 bot.command('conversions', ctx => ctx.reply(
     telegrafResponse.conversions()
-));
-
-bot.command('language', ctx => ctx.reply(
-
 ));
 
 bot.command('currencies', ctx => ctx.reply(
     telegrafResponse.currencies()
 ));
 
-bot.command('USD_to_RUB', ctx => ctx.reply(
-    conversion = getRandomInt(10)
+bot.command('languages', ctx => ctx.reply(
+    telegrafResponse.languages()
 ));
+
+const languageList = constants.getLanguageList();
+
+bot.command(languageList, ctx => {
+    const language = ctx.update.message.text.split('/')[1];
+    ctx.reply(telegrafResponse.changeLanguage(language));
+});
+
+
+const conversions = constants.getConversionsList();
+
+bot.command(conversions, ctx => {
+    const conversion = ctx.update.message.text.split('/')[1];
+    ctx.reply(telegrafResponse.saveConversion(conversion));
+});
 
 bot.on('text', ctx => {
         const userText = ctx.message.text;
-        const number = parseInt(userText);
-        ctx.reply(conversion * number);
+        ctx.reply(telegrafResponse.convert(userText));
     }
 );
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-// bot.help((ctx) => ctx.reply(`А тут будет полный список валют :)
-// ${constants.currencyList.keys().toString()}
-// `));
-//
-// bot.on('text', async (ctx) => {
-//     try {
-//         const userText = ctx.message.text;
-//
-//         let value = userText.split(' ')[0];
-//         let currency = userText.split(' ')[1];
-//         let defaultMode = false;
-//
-//         if(!currency){ // || !constants.currencyList.has(currency)) {
-//             defaultMode = true;
-//             currency = 'RUB' ;
-//         }
-//         // if(typeof value!== 'number' || value<0) {
-//         //     defaultMode = true;
-//         //     value = 1;
-//         // }
-//         converter.convert(value, currency).then(response => {
-//             let symbol = currencySymbols[currency];
-//             symbol = symbol ? symbol : currency;
-//             console.log(response);
-//             console.log(symbol);
-//             ctx.reply(parseFloat(response).toFixed(2) +' '+ symbol);
-//         });
-//
-//     } catch(e) {
-//         ctx.reply('Непрааавильно /help');
-//     }
-// });
 
 bot.launch();
--- telegraf-request-handler.js
+++ telegraf-request-handler.js
@@ -1,68 +1,111 @@
-import {readFile} from "./utils/file-reader-writer.js";
-import config from "./config.js";
-import responseTexts from "./response-texts.js";
 import commands from "./commands.js";
 import currenciesInfo from './currencies-info.js';
+import constants from "./constants.js";
+import conversionParser from './conversion-parser.js';
 
+//TODO solve problem with origin and custom language
 let language = 'eng';
+let customLanguage = false;
 let name = null;
 let currentConversion = null;
-let currencyRates = null;
-
-const getCurrencyRates = () => {
-    if (!currencyRates) {
-        currencyRates = readFile(config.files.dataStorage.currency);
-    }
-    return currencyRates;
-};
 
 const start = (userInfo) => {
-    userInfo.first_name ? name = userInfo.first_name : name = null;
-    language = userInfo.language_code;
+    if (userInfo.first_name) {
+        name = userInfo.first_name;
+    }
 
-    let response = `${responseTexts.hello[language]}${name ? ', ' + name : ''}!`;
-    response += responseTexts.start[language];
+    const availableLanguages = constants.getLanguageList();
+    if (availableLanguages.includes(userInfo.language_code)) {
+        language = userInfo.language_code;
+    }
+
+    let response = `${commands.start.hello[language]}${name ? ', ' + name : ''}!\n`;
+    response += commands.start.text[language];
 
     return response;
 };
 
 const help = () => {
+    let response = commands.help.text[language];
+    const customCommands = commands.customCommands;
+    customCommands.forEach(command => {
+        response += `${commands[command].name} - ${commands[command].description[language]}\n`;
+    });
+    return response;
 };
 
 const conversions = () => {
-    let response = commands.conversions.text[language];
-    const conversionsList = getConversionsList();
-    conversionsList.forEach((conversion) => {
-        response += `/${conversion}\n`;
-    });
+    let response = commands.conversions.text[language] + '\n';
+    response += makeConversionsListAsCommands();
     return response;
+};
+
+const makeConversionsListAsCommands = () => {
+    let list = '';
+    const conversionsList = constants.getConversionsList();
+    conversionsList.forEach((conversion) => {
+        list += `/${conversion}\n`;
+    });
+    return list;
 };
 
 const currencies = () => {
     let response = '';
-    currenciesInfo.forEach(currency=>{
-       response += `${currency.code} - ${currency.name[language]} (${currency.symbol})\n`;
+    currenciesInfo.forEach(currency => {
+        response += `${currency.code} - ${currency.name[language]} (${currency.symbol})\n`;
     });
     return response;
 };
 
-const getConversionsList = () => {
-    const rates = getCurrencyRates();
-    const availableConversions = Object.values(rates);
-    let conversionsList = [];
-    availableConversions.forEach((conversion) => {
-        const from = conversion.from;
-        const to = conversion.to;
-        const conversionName = `${from}_to_${to}`;
-        conversionsList.push(conversionName);
+const languages = () => {
+    const languageList = constants.getLanguageList();
+    let response = `${commands.language.text[language]}\n`;
+    languageList.forEach(language => {
+        response += `/${language}\n`;
     });
-    return conversionsList;
+    return response;
+};
+
+const changeLanguage = (language) => {
+    //TODO check this
+    this.language = language;
+    return commands.changeLanguage.text[language];
+};
+
+const saveConversion = (conversion) => {
+    const conversionName = conversionParser.parse(conversion);
+    currentConversion = constants.getCurrencyRates()[conversionName];
+    return commands.saveConversion.text[language] + conversion;
+};
+
+const convert = (userNumber) => {
+    let response = '';
+    if (!currentConversion) {
+        response = commands.convert.failedConversion[language];
+        response += makeConversionsListAsCommands();
+    }
+
+    const number = Math.abs(parseFloat(userNumber));
+    if (isNaN(number)) {
+        response += commands.convert.invalidNumber[language];
+    } else {
+        const rate = currentConversion.rate;
+        const result = number * rate.toFixed(2);
+        //TODO get back commit about array structure
+        const from = currenciesInfo[currentConversion.from];
+        const to = currenciesInfo[currentConversion.to];
+        response += `${number}${from.symbol} = ${result}${to.symbol}`;
+    }
+    return response;
 };
 
 export default {
     start: start,
     help: help,
     conversions: conversions,
-    getConversionsList: getConversionsList,
-    currencies:currencies
+    currencies: currencies,
+    languages: languages,
+    changeLanguage: changeLanguage,
+    saveConversion: saveConversion,
+    convert: convert
 }
