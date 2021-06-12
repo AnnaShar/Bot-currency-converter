9a008a8b0e8239422a6523e60cc836e61177f426
Anna Sharuntsova <Anna.Sharuntsova@confirmit.com>
2021-06-12 17:32:03 +0300

    Add user info tracking to save it between sessions.

---
+++ user-info.js
@@ -0,0 +1,72 @@
+import {readFile, writeFile} from "./utils/file-reader-writer.js";
+import config from "./config.js";
+import constants from "./constants.js";
+
+let userInfo = null;
+
+const defaultInfo = {
+    language: 'eng'
+};
+
+const getUserInfo = (id) => {
+    if (!userInfo) {
+        if (!id) {
+            return defaultInfo;
+        }
+
+        let userInfoFromFile;
+        try {
+            userInfoFromFile = readFile(`${config.files.users}/user_${id}.json`);
+        } catch (e) {
+            return defaultInfo;
+        }
+        if (userInfoFromFile) {
+            userInfo = userInfoFromFile;
+        }
+    }
+    return userInfo;
+};
+
+const saveUserInfo = (id, info) => {
+    const parsedInfo = parseUserInfo(info);
+    userInfo = parsedInfo;
+    writeFile(`${config.files.users}/user_${id}.json`, parsedInfo);
+    return userInfo;
+};
+
+const parseUserInfo = (info) => {
+    let language;
+    const availableLanguages = constants.getLanguageList();
+    if (availableLanguages.includes(info.language_code)) {
+        language = info.language_code;
+    } else {
+        language = defaultInfo.language;
+    }
+
+    const userInfo = {
+        id: info.id,
+        language: language,
+        firstName: info.first_name,
+        lastName: info.last_name
+    };
+
+    return userInfo;
+};
+
+const updateUserInfo = (id, newUserInfo) => {
+    const oldUserInfo = getUserInfo(id);
+    const updatedInfo = {
+        ...oldUserInfo,
+        ...newUserInfo
+    };
+
+    userInfo = updatedInfo;
+    writeFile(`${config.files.users}/user_${id}.json`, updatedInfo);
+    return userInfo;
+};
+
+export default {
+    getUserInfo,
+    saveUserInfo,
+    updateUserInfo
+}
